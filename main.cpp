19f86e1dfb2d07d6150b1733f785f55c1dd5f40f
Turupawn <antares_43@hotmail.com>
2013-11-13 19:04:34 -0600

    Primer commit

--- main.cpp
+++ main.cpp
@@ -1,46 +1,91 @@
-#include <iostream>
 #include <vector>
-using namespace std;
 
-#include "Personaje.h"
-#include "Curador.h"
-#include "Tanque.h"
+
+//#include "Nodo.h"
+//#include "Lista.h"
+
+//#include "ArbolBinario.h"
+
+//bool encontrada=false;
+//
+//bool puedoLLegar(char tablero[5][5],int x_actual, int y_actual,int pasos)
+//{
+//    //Casos base
+//    if(pasos<0)
+//        return false;
+//
+//    if(x_actual>=5
+//       || y_actual>=5
+//       || x_actual<0
+//       || y_actual<0)
+//       return false;
+//
+//    if(tablero[y_actual][x_actual]=='L')
+//        encontrada=true;
+//
+//    if(tablero[y_actual][x_actual]=='#')
+//       return false;
+//
+//    if(tablero[y_actual][x_actual]=='S' && encontrada)
+//       return true;
+//
+//    pasos--;
+//
+//    return puedoLLegar(tablero,x_actual+1,y_actual,pasos)
+//            || puedoLLegar(tablero,x_actual-1,y_actual,pasos)
+//            || puedoLLegar(tablero,x_actual,y_actual+1,pasos)
+//            || puedoLLegar(tablero,x_actual,y_actual-1,pasos);
+//}
+
+#include <iostream>
+#include <fstream>
+using namespace std;
 
 int main()
 {
-    vector<Personaje*>personajes;
+    ofstream out("ejemplo.txt");
+    out<<"Escribiendo en un archivo.";
+    out.close();
+
+    ifstream in("ejemplo.txt");
+    string str;
+    getline(in,str);
+
+    cout<<"Lei: "<<str;
+
+//    char tablero[5][5]={{' ','#',' ',' ',' '},
+//                        {'E','#',' ','#','S'},
+//                        {' ','#',' ','#',' '},
+//                        {' ','#',' ','#','#'},
+//                        {' ',' ',' ',' ','L'}};
+//
+//    cout<<puedoLLegar(tablero,0,1,12);
 
 
-    personajes.push_back(new Curador(70,10,15));
-    personajes.push_back(new Tanque(200,3));
 
-    for(int i=0;i<personajes.size();i++)
-    {
-        personajes[i]->imprimir();
-    }
-    cout<<"---------Activar Poder---------"<<endl;
+//    for(int i=0;i<5;i++)
+//    {
+//        for(int j=0;j<5;j++)
+//        {
+//            cout<<tablero[i][j];
+//        }
+//        cout<<endl;
+//    }
+//
+//    return false;
 
-    for(int i=0;i<personajes.size();i++)
-    {
-        personajes[i]->activarPoder();
-    }
 
-    for(int i=0;i<personajes.size();i++)
-    {
-        personajes[i]->imprimir();
-    }
+//    ArbolBinario arbol;
+//    arbol.imprimir(arbol.padre);
 
-    cout<<"---------Recibir ataque---------"<<endl;
-
-    for(int i=0;i<personajes.size();i++)
-    {
-        personajes[i]->recibirAtaque(10);
-    }
-
-    for(int i=0;i<personajes.size();i++)
-    {
-        personajes[i]->imprimir();
-    }
-
+////  Listas enlazadas
+//    Lista lista;
+//    lista.agregarNodo(new Nodo(100));
+//    lista.agregarNodo(new Nodo(300));
+//    lista.agregarNodo(new Nodo(400));
+//
+//    lista.agregarNodo(new Nodo(200),0);
+//
+//    lista.imprimirLista();
     return 0;
 }
